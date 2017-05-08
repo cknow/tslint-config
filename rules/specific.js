5c02727cc0a9e5749adb76dc433a15aaab14eb1f
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-05-08 16:10:21 -0300

    fix(rules): Allow options in rule `only-arrow-funtions` and remove `variable-declaration` of rule `t

--- rules/specific.js
+++ rules/specific.js
@@ -35,7 +35,7 @@ module.exports = {
         'no-reference': true,
         'no-unnecessary-type-assertion': true,
         'no-var-requires': true,
-        'only-arrow-functions': true,
+        'only-arrow-functions': [true, 'allow-declarations', 'allow-named-functions'],
         'prefer-for-of': true,
         'promise-function-async': true,
         'typedef': [true,
@@ -44,7 +44,6 @@ module.exports = {
             'parameter',
             'arrow-parameter',
             'property-declaration',
-            'variable-declaration',
             'member-variable-declaration',
             'object-destructuring',
             'array-destructuring'
