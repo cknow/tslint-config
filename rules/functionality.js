a1f195b55e05b173a120403e4925b8863385ca42
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-05-23 16:20:24 -0300

    feat(functionality): Added new rule `prefer-conditional`

--- rules/functionality.js
+++ rules/functionality.js
@@ -39,6 +39,7 @@ module.exports = {
         'no-use-before-declare': true,
         'no-var-keyword': true,
         'no-void-expression': false,
+        'prefer-conditional': true,
         'prefer-object-spread': true,
         'radix': true,
         'restrict-plus-operands': true,
